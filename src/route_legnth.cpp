47184dafffba05ce671eef91d046e441bf141ffc
Hanpu Liu <26217378+hanpuliu-charles@users.noreply.github.com>
2022-11-10 13:43:17 -0600

    empty routeLength func

---
+++ src/route_legnth.cpp
@@ -0,0 +1,5 @@
+#include "route_length.h"
+
+int routeLength(std::pair<int,int> loc1, std::pair<int,int> loc2){
+    return 0;
+}
---
+++ src/route_length.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <utility>
+// using std::pair;
+
+int routeLength(std::pair<int,int> loc1, std::pair<int,int> loc2);

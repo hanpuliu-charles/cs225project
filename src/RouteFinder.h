13bfacdec27adef79c309b838aec3cc90b8c69bb
hanpuliu-charles <26217378+hanpuliu-charles@users.noreply.github.com>
2022-11-16 21:02:06 -0600

    Started on airport and route class

--- src/RouteFinder.h
+++ src/RouteFinder.h
@@ -1,6 +1,26 @@
 #pragma once
+#include <string>
+
 
 // This is the main part of the project
 class RouteFinder {
+    public:
 
+    private:
+    
+
+
+    class Airport{
+        std::string airportName_;
+        
+
+    };
+    class Route{
+        std::string origin_;
+        std::string dest_;
+        int dist_;
+        
+    };
+    
 };
+

29d5319976bc718fe8cc7ad3cc611c5d6d0a5cc1
peterchong <peterrchongg@outlook.com>
2022-11-17 19:36:30 -0600

    new

--- src/utils/airport.h
+++ src/utils/airport.h
@@ -1,2 +1,23 @@
-#include "airport.cpp"
+#pragma once
+#include <string>
+
+using namespace std;
+class Airport
+{
+
+public:
+    Airport();
+    Airport(const Airport& other);
+    Airport(unsigned ID, string name, string IATAcode, double latitude, double longitude, string city, string country, double altitude);
+    ~Airport();
+
+    unsigned getAirportID() const;
+    string getAirportName() const;
+    string getAirportIATACode() const;
+    bool operator==(const Airport& other) const;
+private:
+    unsigned airport_ID;    
+    string airport_name;    
+    string airport_IATAcode;    
+};
 

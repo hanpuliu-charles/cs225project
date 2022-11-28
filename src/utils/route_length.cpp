2bbe0b24f406b1be813919b775ed76182c7d4239
hanpuliu-charles <26217378+hanpuliu-charles@users.noreply.github.com>
2022-11-27 22:02:52 -0600

    Added code to produce all route length of routes,

--- src/utils/route_length.cpp
+++ src/utils/route_length.cpp
@@ -1,8 +1,39 @@
 #include "route_length.h"
 #include <cmath>
+#include <string>
+#include <unordered_map>
+#include <fstream>
+#include "utils/misc.h"
+#include <utility>
+#include <iostream>
 
+int main(){
+    std::string airports_filename = "";
+    std::string routes_filename = "";
+    std::ifstream ifs;
+    ifs.open(airports_filename);
+    std::unordered_map<std::string, std::pair<long double, long double>> airportToCoord;
+    std::string line;
+    while(ifs>>line){
+        std::vector<std::string> brokenUpString = delimitString(line,',');
+        std::string airport_code = brokenUpString[4].substr(1, brokenUpString[4].length()-2);
+        long double lat = std::stold(brokenUpString[6]);
+        long double lon = std::stold(brokenUpString[7]);
+        airportToCoord[airport_code]=std::make_pair(lat, lon);
+    }
 
-int routeLength(std::pair<int,int> loc1, std::pair<int,int> loc2){
+    // Now we have all coord of airports, we can go through routes list
+    
+    ifs.open(routes_filename);
+    while(ifs>>line){
+        std::vector<std::string> brokenUpString = delimitString(line,',');
+        std::string origin = brokenUpString[2]; // NOTE: all airport using 3 letter IATA names in routes
+        std::string dest = brokenUpString[4];
+        std::cout << routeLength(airportToCoord[origin], airportToCoord[dest]) << std::endl;
+        // airportToCoord[airport_code]=std::make_pair(lat, lon);
+    }
+}
+int routeLength(std::pair<long double,long double> loc1, std::pair<long double, long double> loc2){
     return 0;
 //first convert both lat and long to radians, lat1,long2 and lat2,long2
 //Use the formula d = 3963.0 * arccos[(sin(lat1) * sin(lat2)) + cos(lat1) * cos(lat2) * cos(long2 – long1)]
